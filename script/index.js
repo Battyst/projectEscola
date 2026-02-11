7a11d7f75cbd9bbfcbd5a1c860e2053d7ba3c93c
battyst <samuelbatistam3@gmail.com>
2026-02-11 11:55:16 -0300

    escola do zé

--- script/index.js
+++ script/index.js
@@ -1,9 +1,27 @@
+let usuarios = [
+  {
+    id: 1,
+    nome: "Samuel Batista",
+    email: "samuelbatistam3gmail.com",
+    senha: "1234"
+  }
+];
+
+
+
+
 function login () {
 const email = document.getElementById("email").value
 const password = document.getElementById("password").value
 
 console.log(email)
 console.log(password)
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
 
 if (email === "samuelbatistam3@gmail.com" && password === "1234") {
   alert("Login efetuado com sucesso")
@@ -12,3 +30,5 @@ if (email === "samuelbatistam3@gmail.com" && password === "1234") {
   alert("Email ou senha incorretos")
 }
 }
+
+
